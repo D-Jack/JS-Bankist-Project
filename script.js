2246ad4d7fc9fd641f946937ead1e1df0ffb46b8
Siddharth <siddharthauchiha50@gmail.com>
2021-06-21 16:24:38 +0530

    apply loan functionality

--- script.js
+++ script.js
@@ -183,6 +183,21 @@ btnTransfer.addEventListener('click', function (e) {
     }
 });
 
+btnLoan.addEventListener('click', function (e) {
+    e.preventDefault();
+    const amount = Number(inputLoanAmount.value);
+    if (
+        amount > 0 &&
+        currentAccount.movements.some(mov => mov >= amount * 0.1)
+    ) {
+        currentAccount.movements.push(amount);
+
+        //Update UI
+        updateUI(currentAccount);
+    }
+    inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e) {
     e.preventDefault();
 
